2718abf476fece1041effd44b1428bd6ea9a0ff2
Dave Walker <david.walker5@icloud.com>
2023-09-04 08:12:17 +0100

    Remove class that's moved to the logic assembly

--- src/BaseStationReader.Terminal/Interfaces/ITrackerSettingsBuilder.cs
+++
@@ -1,9 +0,0 @@
-﻿using BaseStationReader.Entities.Config;
-
-namespace BaseStationReader.Terminal.Interfaces
-{
-    internal interface ITrackerSettingsBuilder
-    {
-        ApplicationSettings? BuildSettings(IEnumerable<string> args, string configJsonPath);
-    }
-}
