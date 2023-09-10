cee89d0b1b29465a706684ff265ba95361bf3e78
Dave Walker <david.walker5@icloud.com>
2023-09-10 08:00:30 +0100

    Remove redundant files

--- src/BaseStationReader.Entities/Interfaces/IAircraftCsvExporter.cs
+++
@@ -1,9 +0,0 @@
-﻿using BaseStationReader.Entities.Tracking;
-
-namespace BaseStationReader.Entities.Interfaces
-{
-    public interface IAircraftCsvExporter
-    {
-        void Export(IEnumerable<Aircraft> aircraft, string fileName, char separator);
-    }
-}
--- src/BaseStationReader.Terminal/Interfaces/ITrackerIndexManager.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BaseStationReader.Terminal.Interfaces
-{
-    internal interface ITrackerIndexManager
-    {
-        void AddAircraft(string address, int rowNumber);
-        int FindAircraft(string address);
-        int RemoveAircraft(string address);
-    }
-}
