d82a86369f7bb279a8bb40b16f819e14fac2e30e
Dave Walker <david.walker5@icloud.com>
2023-09-21 11:25:34 +0100

    Added aircraft generator tests

---
+++ src/BaseStationReader.Tests/AircraftGeneratorTest.cs
@@ -0,0 +1,23 @@
+﻿using BaseStationReader.Logic.Simulator;
+using BaseStationReader.Tests.Mocks;
+
+namespace BaseStationReader.Tests
+{
+    [TestClass]
+    public class AircraftGeneratorTest
+    {
+        [TestMethod]
+        public void GenerateNewAircraftTest()
+        {
+            var logger = new MockFileLogger();
+            var generator = new AircraftGenerator(logger);
+            var aircraft = generator.Generate(null);
+
+            Assert.IsNotNull(aircraft);
+            Assert.AreEqual(6, aircraft.Address.Length);
+            Assert.AreEqual(4, aircraft.Squawk?.Length);
+            Assert.IsNotNull(aircraft.FirstSeen);
+            Assert.IsNotNull(aircraft.LastSeen);
+        }
+    }
+}
