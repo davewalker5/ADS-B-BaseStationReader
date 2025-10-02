b2a6bd19e5c6d1cdc6bd7e6655b64f9d195a43c7
Dave Walker <david.walker5@icloud.com>
2025-10-02 16:57:33 +0100

    Simplify external API wrapper interface

--- src/BaseStationReader.Interfaces/Api/IExternalApiWrapper.cs
+++ src/BaseStationReader.Interfaces/Api/IExternalApiWrapper.cs
@@ -5,18 +5,15 @@ namespace BaseStationReader.Interfaces.Api
 {
     public interface IExternalApiWrapper
     {
-        Task<Flight> LookupActiveFlightAsync(string address, IEnumerable<string> departureAirportCodes, IEnumerable<string> arrivalAirportCodes);
-        Task<Flight> LookupHistoricalFlightAsync(string address, IEnumerable<string> departureAirportCodes, IEnumerable<string> arrivalAirportCodes);
-        Task<List<Flight>> LookupActiveFlightsInBoundingBox(double centreLatitude, double centreLongitude, double rangeNm);
-        Task<Airline> LookupAirlineAsync(string icao, string iata, string name);
-        Task<Aircraft> LookupAircraftAsync(string address, string alternateModelICAO);
-        Task<IEnumerable<string>> LookupAirportWeather(string icao);
-
         Task<bool> LookupAsync(
             ApiEndpointType type,
             string address,
             IEnumerable<string> departureAirportCodes,
             IEnumerable<string> arrivalAirportCodes,
             bool createSighting);
+
+        Task<List<Flight>> LookupActiveFlightsInBoundingBox(double centreLatitude, double centreLongitude, double rangeNm);
+
+        Task<IEnumerable<string>> LookupAirportWeather(string icao);
     }
 }
