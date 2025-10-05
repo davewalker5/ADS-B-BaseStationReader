46f138a0b1aade2a5e4fef5b5da723de705026da
Dave Walker <david.walker5@icloud.com>
2025-10-05 08:36:25 +0100

    Added flight number conversion export

--- src/BaseStationReader.Lookup/Logic/FlightNumberExportHandler.cs
+++ src/BaseStationReader.Lookup/Logic/FlightNumberExportHandler.cs
@@ -1,6 +1,8 @@
 using BaseStationReader.BusinessLogic.Api;
 using BaseStationReader.BusinessLogic.Api.Wrapper;
 using BaseStationReader.BusinessLogic.Configuration;
+using BaseStationReader.BusinessLogic.Database;
+using BaseStationReader.BusinessLogic.Export;
 using BaseStationReader.Entities.Config;
 using BaseStationReader.Entities.Logging;
 using BaseStationReader.Interfaces.Database;
@@ -27,8 +29,21 @@ namespace BaseStationReader.Lookup.Logic
             Logger.LogMessage(Severity.Info, $"Using the {_serviceType} API");
 
             // Configure the external API wrapper
-            var wrapper = ExternalApiFactory.GetWrapperInstance(Logger, TrackerHttpClient.Instance, Context, null, _serviceType, ApiEndpointType.ActiveFlights, Settings);
+            var trackedAircraftWriter = new TrackedAircraftWriter(Context);
+            var wrapper = ExternalApiFactory.GetWrapperInstance(Logger, TrackerHttpClient.Instance, Context, trackedAircraftWriter, _serviceType, ApiEndpointType.ActiveFlights, Settings);
 
+            // Get a list of callsign to flight number conversions for tracked flights
+            var numbers = await wrapper.GetFlightNumbersForTrackedAircraftAsync([]);
+            if (numbers?.Count > 0)
+            {
+                // Exctract the CSV file path from the command line arguments and export the data
+                var filePath = Parser.GetValues(CommandLineOptionType.ConvertCallsigns)[0];
+                new FlightNumberExporter().Export(numbers, filePath);
+            }
+            else
+            {
+                Console.WriteLine($"No flight numbers returned");
+            }
         }
     }
 }
