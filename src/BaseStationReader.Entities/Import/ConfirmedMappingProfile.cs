460b3dc366717e4e56c0cae21c586faa85acd46e
Dave Walker <david.walker5@icloud.com>
2025-10-07 10:58:40 +0100

    Added build and import scripts for the model

--- src/BaseStationReader.Entities/Import/ConfirmedMappingProfile.cs
+++ src/BaseStationReader.Entities/Import/ConfirmedMappingProfile.cs
@@ -12,7 +12,7 @@ namespace BaseStationReader.Entities.Import
             Map(m => m.AirlineICAO).Name("airline_icao");
             Map(m => m.AirlineIATA).Name("airline_iata");
             Map(m => m.FlightIATA).Name("iata_flight");
-            Map(m => m.Callsign).Name("callSign");
+            Map(m => m.Callsign).Name("callsign");
             Map(m => m.Digits).Name("digits");
         }
     }
