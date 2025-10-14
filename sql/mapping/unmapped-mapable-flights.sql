2fd60deb736da80033e47d4188e66503e6a7162c
Dave Walker <david.walker5@icloud.com>
2025-10-14 10:58:26 +0100

    Fixup typo in unmapped mapable flights query

--- sql/mapping/unmapped-mapable-flights.sql
+++ sql/mapping/unmapped-mapable-flights.sql
@@ -1,4 +1,15 @@
-SELECT      ta.Address, ta.Callsign, fnm.FlightIATA, ta.LastSeen
+SELECT      ta.Address,
+            ta.Callsign,
+            fnm.AirlineIATA,
+            fnm.AirlineICAO,
+            fnm.AirlineName,
+            fnm.AirportIATA,
+            fnm.AirportICAO,
+            fnm.AirportName,
+            fnm.Embarkation,
+            fnm.Destination,
+            fnm.FlightIATA,
+            ta.LastSeen
 FROM        TRACKED_AIRCRAFT ta
 INNER JOIN  FLIGHT_NUMBER_MAPPING fnm ON fnm.Callsign = ta.Callsign
 WHERE       ta.LookupTimestamp IS NULL;
