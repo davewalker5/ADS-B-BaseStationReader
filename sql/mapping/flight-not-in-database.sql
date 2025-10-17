4ace02e6c846be040c27d6878d3d4128ba18d96c
Dave Walker <david.walker5@icloud.com>
2025-10-17 14:40:56 +0100

    Complete implementation of aircraft exclusions

--- sql/mapping/flight-not-in-database.sql
+++ sql/mapping/flight-not-in-database.sql
@@ -2,6 +2,8 @@ SELECT          ta.Callsign,
                 ta.LastSeen
 FROM            TRACKED_AIRCRAFT ta
 LEFT OUTER JOIN FLIGHT_NUMBER_MAPPING fnm ON fnm.Callsign = ta.Callsign
+LEFT OUTER JOIN EXCLUDED_ADDRESS ea ON ea.Address = ta.Address
 WHERE           ta.LookupTimestamp IS NULL
+AND             ea.Id IS NULL
 AND             LENGTH( IFNULL( ta.Callsign, '' )) > 0
 AND             fnm.Id IS NULL;
